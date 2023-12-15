5ac9e2f6f177de73cb370f998a536242a1f77137
luna <lunasumshine@gmail.com>
2023-12-15 23:10:39 +0800

    test for my simple_test allocator using map,unordered_map,vector

--- test/test_allocator.cpp
+++ test/test_allocator.cpp
@@ -1,12 +1,39 @@
 #include "../allocator.h"
+#include "../sgi_allocator.h"
 
+#include <string>
 #include <vector>
+#include <map>
+#include <unordered_map>
 #include <algorithm>
 using namespace std;
 
 int main(){
-    int v[3] = {1,3,4};
-    std::vector<int,ZMJ::allocator<int>> vec(v,v+3);
+    //for vector
+    int v[] = {1,3,4,13203,2302,23,34,3,53,23,23};
+    std::vector<int,ZMJ::simple_alloc<int>> vec(v,v+sizeof(v)/sizeof(int));
     for_each(vec.begin(),vec.end(),[](int i){ cout<<i<<" "; });
+    std::cout<<std::endl;
+
+    //for map
+    std::map<std::string,int,std::less<std::string>,ZMJ::simple_alloc<std::pair<const std::string,int>>> mp;
+    std::string base = "HUTAO";
+    mp.insert({"ZMJ",23});
+    for(int i = 0; i < 1000; ++i){
+        char buf[20] = {0};
+        std::string tmp = base + itoa(i,buf,10);
+        mp.insert({tmp,16});
+    }
+    for_each(mp.begin(),mp.end(),[](std::pair<std::string,int> i){ cout<<i.first<<" "<<i.second<<"\n"; });
+
+    //for unordered_map : 这里还要给simple_alloc提供一个拷贝构造！simple_alloc(const simple_alloc<U>& other);
+    std::unordered_map<std::string,int,hash<std::string>,equal_to<std::string>,ZMJ::simple_alloc<std::pair<const std::string,int>>> umap;
+    for(int i = 0; i < 1000; ++i){
+        char buf[20] = {0};
+        std::string tmp = base + itoa(i,buf,10);
+        umap.insert({tmp,16});
+    }
+    for_each(umap.begin(),umap.end(),[](std::pair<std::string,int> i){ cout<<i.first<<" "<<i.second<<"\n"; });
+
     return 0;
 }
