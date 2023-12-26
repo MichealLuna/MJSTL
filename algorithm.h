fd899d424772f6d0c38cc7ad2232f3c177496ffc
luna <lunasumshine@gmail.com>
2023-12-26 20:09:13 +0800

    create algorithm

---
+++ algorithm.h
@@ -0,0 +1,10 @@
+#ifndef __ALGORITHM_H__
+#define __ALGORITHM_H__
+
+#include "algo.h"
+#include "algobase.h"
+#include "numeric.h"
+#include "heap_algo.h"
+#include "set_algo.h"
+
+#endif//!__ALGORITHM_H__
