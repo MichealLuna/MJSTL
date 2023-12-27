34893849f6183dc35641016ec1511597b16e741a
luna <lunasumshine@gmail.com>
2023-12-27 19:41:21 +0800

    create memory.h

---
+++ memory.h
@@ -0,0 +1,10 @@
+#ifndef __MEMORY_H__
+#define __MEMORY_H__
+
+#include "algobase.h"
+#include "allocator.h"
+#include "sgi_allocator.h"
+#include "uninitialized.h"
+#include "tempbuf.h"
+
+#endif//!__MEMORY_H__
