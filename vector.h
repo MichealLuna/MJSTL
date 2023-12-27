1489d401d702bf9af4d8180e637ed1d814b40af7
luna <lunasumshine@gmail.com>
2023-12-28 00:17:01 +0800

    update vector.h

--- vector.h
+++ vector.h
@@ -12,144 +12,68 @@ namespace ZMJ{
 template <typename T,typename Alloc = simple_alloc<T>>
 class vector{
 public:
-    using value_type            = T;
-    using pointer               = T*;
-    using reference             = T&;
-    using const_pointer         = const T*;
-    using const_reference       = const T&;
-    using iterator              = T*;
-    using const_iterator        = const T*;
-    using size_type             = size_t;
-    using difference_type       = ptrdiff_t;
+    typedef T                                   value_type;
+    typedef Alloc                               allocate_type;
+    typedef value_type*                         pointer;
+    typedef const value_type*                   const_pointer;
+    typedef value_type&                         reference;
+    typedef const value_type&                   const_reference;
+    typedef size_t                              size_type;
+    typedef ptrdiff_t                           difference_type;
 
+    /*iterator_type*/
+    typedef value_type*                         iterator;
+    typedef const value_type*                   const_iterator;
+    typedef reverse_iterator<const_iterator>    const_reverse_iterator;
+    typedef reverse_iterator<iterator>          reverse_iterator;
 protected:
-    using data_allocator        = Alloc;
-    using reverse_iterator      = reverse_iterator<iterator>;
+    typedef Alloc                               data_allocator;
 
-    using self                  = vector<T,Alloc>;
 protected:
     iterator start;
     iterator finish;
     iterator end_of_storage;
 
-    /*插入元素*/
-    void insert_aux(iterator position,const T& x);
-
-    /*初始化：插入n个值为value的元素。*/
-    void deallocate(){
-        if(start)
-            data_allocator::deallocate(start,end_of_storage-start);
-    }
-
-    /*初始化：插入n个值为value的元素。*/
-    void fill_initialize(size_type n,const T& value){
-        start = allocate_and_fill(n,value);
-        finish = start + n;
-        end_of_storage = finish;
-    }
-
-    /*配置空间并填充内容*/
-    iterator allocate_and_fill(size_type n,const T& value){
-        iterator result = data_allocator::allocate(n);
-        uninitialized_fill_n(result,n,value);
-        return result;
-    }
-
-    /*配置空间并复制内容*/
-    iterator allocate_and_copy(size_type n,const_iterator first,const_iterator last){
-        iterator result = data_allocator::allocate(n);
-        try{
-            /*将原来[first,last)复制到last中去。*/
-            uninitialized_copy(first,last,result);
-            return result;
-        }
-    }
-
 public:
+    /*construct,assignment,destruct*/
     vector():start(nullptr),finish(nullptr),end_of_storage(nullptr){}
-
-    vector(size_t n, const T& value){
-        fill_initialize(n,value);
-    }
-
-    vector(long n,const T& value){
-        fill_initialize(n,value);
-    }
-
-    explicit vector(size_type n){
-        fill_initialize(n,T());
-    }
+    explicit vector(size_type n){ allocate_and_fill(n,T()); }
+    vector(size_type n, const T& value){ allocate_and_fill(n,value); }
+    template<class InputIterator>
+    vector(InputIterator first,InputIterator last);
 
     /*copy construct*/
-    vector(const vector<T,Alloc>& rhs){
-        fill_initialize(x.size(),T());
-        finish = uninitialized_copy(x.begin(),x.end(),start);
-    }
+    vector(const vector<T,Alloc>& x);
+    vector(vector<T,Alloc>&& x);
 
-    /*assignment copy*/
-    vector<T,Alloc>& operator=(const vector<T,Alloc>& rhs);
+    /*assignment operator*/
+    vector<T,Alloc>& operator=(const vector<T,Alloc>& x);
+    vector<T,Alloc>& operator=(vector<T,Alloc>&& x);
 
-    vector(vector<T,Alloc>&& rhs){
-        start = rhs.start;
-        finish = rhs.finish;
-        end_of_storage = rhs.end_of_storage;
-        rhs.start = nullptr;
-        rhs.finish = nullptr;
-        rhs.end_of_storage = nullptr;
-    }
-
-    ~vector(){
-        destory(start,finish);
-        deallocate();
-    }
-
+    /*destrust*/
+    ~vector(){ destory_and_deallocate(); }
 public:
-    iterator begin() const{
-        return start;
-    }
+    /*about iterator*/
+    iterator begin() { return start; }
+    const_iterator begin() const { return start; }
+    iterator end() { return finish; }
+    const_iterator end() const{ return finish; }
+    reverse_iterator rbegin() { return reverse_iterator(end());}
+    const_reverse_iterator rbegin() const{ return const_reverse_iterator(end());}
+    reverse_iterator rend() { return reverse_iterator(begin());}
+    const_reverse_iterator rend() const{ return const_reverse_iterator(begin());}
 
-    iterator end() const{
-        return finish;
-    }
+    /*about containter*/
+    size_type size() const{ return size_type(end() - begin());}
+    size_type max_size() const{ return size_type(-1)/sizeof(T);}
+    size_type capacity() const{ return size_type(end_of_storage - begin());}
+    bool empty() const{ return begin() == end();}
+    void resize(size_type new_size,const T& value);
+    void resize(size_type new_size){ return resize(new_size,T());}
 
-    /*想了想还是使用函数调用获得容器好，利于更改。*/
-    reverse_iterator rbegin() const{
-        return reverse_iterator(end());
-    }
-
-    reverse_iterator rend() const{
-        return reverse_iterator(begin());
-    }
-
-    size_type size() const{
-        return size_type(end() - begin());
-    }
-
-    size_type max_size() const{
-        return size_type(-1)/sizeof(T);
-    }
-
-    size_type capacity() const{
-        return size_type(end_of_storage - begin());
-    }
-
-    bool empty() const{
-        return begin() == end();
-    }
-
-    void reserve(size_type n){
-
-    }
-    void resize(size_type n);
-    void resize(size_type n, const T& value);
-
-    reference operator[](size_type n){
-        return *(begin() + n);
-    }
-    const_reference operator[](size_type n) const{
-        return (const_reference)start[n];
-    }
-
+    /*access container*/
+    reference operator[](size_type n){ return *(begin() + n);}
+    const_reference operator[](size_type n) const{ return (const_reference)start[n];}
     reference at(size_type n){
         assert(start <= start + n && start + n < end_of_storage);
         return (*this)[n];
@@ -158,151 +82,190 @@ public:
         assert(start <= start + n && start + n < end_of_storage);
         return (*this)[n];
     }
+    reference front(){ return *begin();}
+    const_reference front() const{ return (const_reference)(*begin());}
+    reference back(){ return *(end() - 1);}
+    const_reference back() const{ return (const_reference)(*(end() - 1));}
+    iterator data() const{ return start;}
 
-    reference front(){
-        return *begin();
-    }
-
-    const_reference front() const{
-        return (const_reference)(*begin());
-    }
-
-    reference back(){
-        return *(end() - 1);
-    }
-
-    const_reference back() const{
-        return (const_reference)(*(end() - 1));
-    }
-
-    iterator data() const{
-        return start;
-    }
-
-    void push_back(const T& value){
-        if(finish != end_of_storage){
-            construct(finish,value);
-            ++finish;
-        }
-        else{
-            insert_aux(end(), value);
-        }
-    }
-
-    void push_back(T&& value){
-        if(finish != end_of_storage){
-            *finish = value;
-            ++finish;//前提是T类型有自己的移动构造，否则无用。
-        }else{
-            insert_aux(end(),value);/*应该转发。*/
-        }
-    }
-
-    void pop_back(){
-        if(finish != start){
-            destory(finish);
-            --finish;//可能析构
-        }
-    }
-
-    iterator earse(iterator position){
-        if(position + 1 != end())
-            copy(position+1,finish,position);
-        /*
-        *   这真的很奇怪，position位置元素不会被覆盖了吗？
-        * 如果担心直接内存copy，而导致position内容被覆盖，并且没有调用它的析构。
-        * 这个不用担心，因为copy里面使用has_trivial_assignment_construct,
-        * 来决定是使用直接内存memmove还是对copy内容使用赋值构造。
-        * 
-        *   至于赋值过程中，对象的资源是释放还是移动，那是类类型作者该考虑的，
-        * 而不是容器作者该考虑的。
-        */
-        --finish;
-        destory(finish);
-        return position;
-    }
-
-    iterator earse(iterator first,iterator last){
-        iterator it = copy(last,finish,first);
-        data_allocator::destory(it,finish);
-        finish = finish - (last - first);
-        return first;
-    }
-
-    void clear(){
-        /*如果T 是has_trivial_destruct，直接finish = start;否则应该对每个元素调用destroy*/
-    }
-
-    void resize(size_type new_size,const T& value){
-        if(new_size < size())
-            earse(begin() + new_size,end());
-        else
-            insert(end(),new_size - size(),value);/*从何处开始，插入多少个，插入值是什么*/
-    }
-
-    void resize(size_type new_size){
-        resize(new_size,T());
-    }
-
+    /*modify container*/
+    void push_back(const T& value);
+    void pop_back();
+    iterator earse(iterator position);
+    iterator earse(iterator first,iterator last);
+    void clear();
     void insert(iterator pos, size_type n,const T& value);
+    inline void swap(vector<T,Alloc>& rhs);
+    inline void swap(vector<T,Alloc>& x,vector<T,Alloc>& y);
 
-    inline void swap(vector<T,Alloc>& rhs){
-        ZMJ::swap(start,rhs.start);
-        ZMJ::swap(finish,rhs.finish);
-        ZMJ::swap(end_of_storage,rhs.end_of_storage);
-    }
+    /*about allocator*/
+    allocate_type get_allocator(){ return allocate_type();}
+protected:
+    template<class Integer>
+    void vector_construct(Integer n,Integer value,__true_type);
+    template<class InputIterator>
+    void vector_construct(InputIterator first,InputIterator last,__false_type);
+    void destory_and_deallocate();
+    void allocate_and_fill(size_type n,const T& value);
+    iterator allocate_and_copy(iterator first,iterator last);
+    void insert_aux(iterator position,const T& value);
+public:
+};
 
-    inline void swap(vector<T,Alloc>& x,vector<T,Alloc>& y){
-        x.swap(y);
-    }
+template<class T,class Alloc>
+template<class InputIterator>
+vector<T,Alloc>::vector(InputIterator first,InputIterator last){
+    typename __is_integer<InputIterator>::is_integer integer;
+    vector_construct(first,last,integer());
+}
 
-    inline bool operator==(vector<T,Alloc>& x,vector<T,Alloc>& y){
-        return x.size() == y.size() && equal(x.begin(),x.end(),y.begin());
-    }
+template<class T,class Alloc>
+template<class Integer>
+void vector<T,Alloc>::vector_construct(Integer n,Integer value,__true_type){
+    allocate_and_fill(n,value);
+}
 
-    inline bool operator<(vector<T,Alloc>& x,vector<T,Alloc>& y){
-        return lexicographical_compare(x.begin(),x.end(),y.begin(),y.end());
-    }
+template<class T,class Alloc>
+template<class InputIterator>
+void vector<T,Alloc>::vector_construct(InputIterator first,
+    InputIterator last,__false_type){
+    allocate_and_copy(first,last);
+}
 
-    template<class T,class Alloc = simple_alloc<T>>
-    inline bool operator==(vector<T,Alloc>& x,vector<T,Alloc>& y){
-        return !(x == y);
-    }
+template <class T, class Alloc>
+vector<T, Alloc>::vector(const vector<T, Alloc>& x) {
+	allocate_and_copy(x.begin(), x.end());
+}
 
-    inline bool operator>(vector<T,Alloc>& x,vector<T,Alloc>& y){
-        return y < x;
-    }
+template <class T, class Alloc>
+vector<T, Alloc>::vector(vector<T, Alloc>&& x) {
+	start = x.start;
+    finish = x.finish;
+    end_of_storage = x.end_of_storge;
+    x.start = x.finish = x.end_of_storage = 0;
+}
 
-    inline bool operator>=(vector<T,Alloc>& x,vector<T,Alloc>& y){
-        return !(x < y);
-    }
-
-    inline bool operator<=(vector<T,Alloc>& x,vector<T,Alloc>& y){
-        return !(y < x);
-    }
-
-    vector<T,Alloc>& operator=(const vector<T>& rhs){
-        if(this == &rhs) return *this;
+template <class T, class Alloc>
+vector<T,Alloc>& vector<T,Alloc>::operator=(const vector<T,Alloc>& x){
+    if(this != &x){
         const auto len = rhs.size();
-
         /*要重分配空间*/
         if(len > capacity()){
-            iterator it = allocate_and_copy(len,rhs.begin(),rhs.end());
-            ZMJ::destory(start,finish);
-            this->deallocate();
-            start = it;
-            end_of_storage = start + len;
+            destory_and_deallocate();
+            allocate_and_copy(x.begin(),x.end());
         }else if(size() >= len){/*不用重新分配空间*/
-            iterator it = copy(rhs.begin(),rhs.end(),start);
+            iterator it = copy(x.begin(),x.end(),start);
             ZMJ::destory(it,finish);
         }else{/*不用重新分配空间，但需要扩充finish。*/
-            copy(rhs.begin(),rhs.begin() + size(),start);
-            ZMJ::uninitialized_copy(rhs.begin() + size(),rhs.end(),finish);
+            copy(x.begin(),x.begin() + size(),start);
+            ZMJ::uninitialized_copy(x.begin() + size(),x.end(),finish);
         }
         finish = start + len;
     }
-};
+    return *this;
+}
 
+template <class T, class Alloc>
+vector<T,Alloc>& vector<T,Alloc>::operator=(vector<T,Alloc>&& x){
+    if(this != &x){
+        destory_and_deallocate();
+        start = x.start;
+        finish = x.finish;
+        end_of_storage = x.end_of_storage;
+        x.start = x.finish = x.end_of_storage = 0;
+    }
+    return *this;
+}
+
+template <class T, class Alloc>
+void vector<T,Alloc>::resize(size_type new_size,const T& value){
+    if(new_size < size())
+        earse(begin() + new_size,end());
+    else
+        insert(end(),new_size - size(),value);/*从何处开始，插入多少个，插入值是什么*/
+}
+
+template <class T, class Alloc>
+void vector<T,Alloc>::push_back(const T& value){
+    if(finish != end_of_storage){
+        construct(finish,value);
+        ++finish;
+    }
+    else{
+        insert_aux(end(), value);
+    }
+}
+
+template <class T, class Alloc>
+void vector<T,Alloc>::pop_back(){
+    if(finish != start){
+        destory(finish);
+        --finish;//可能析构
+    }
+}
+
+template <class T, class Alloc>
+vector<T,Alloc>::iterator 
+vector<T,Alloc>::earse(iterator position){
+    if(position + 1 != end())
+        copy(position+1,finish,position);
+    /*
+    *   这真的很奇怪，position位置元素不会被覆盖了吗？
+    * 如果担心直接内存copy，而导致position内容被覆盖，并且没有调用它的析构。
+    * 这个不用担心，因为copy里面使用has_trivial_assignment_construct,
+    * 来决定是使用直接内存memmove还是对copy内容使用赋值构造。
+    * 
+    *   至于赋值过程中，对象的资源是释放还是移动，那是类类型作者该考虑的，
+    * 而不是容器作者该考虑的。
+    */
+    --finish;
+    destory(finish);
+    return position;
+}
+
+template <class T, class Alloc>
+vector<T,Alloc>::iterator 
+vector<T,Alloc>::earse(iterator first,iterator last){
+    iterator it = copy(last,finish,first);
+    data_allocator::destory(it,finish);
+    finish = finish - (last - first);
+    return first;
+}
+
+template <class T, class Alloc>
+void vector<T,Alloc>::swap(vector<T,Alloc>& rhs){
+    ZMJ::swap(start,rhs.start);
+    ZMJ::swap(finish,rhs.finish);
+    ZMJ::swap(end_of_storage,rhs.end_of_storage);
+}
+
+template <class T, class Alloc>
+void vector<T,Alloc>::swap(vector<T,Alloc>& x,vector<T,Alloc>& y){
+    x.swap(y);
+}
+
+template <class T, class Alloc>
+void vector<T,Alloc>::destory_and_deallocate(){
+    destory(start,finish);
+    if(start) data_allocator::deallocate(start,end_of_storage - start);
+}
+
+template <class T, class Alloc>
+void vector<T,Alloc>::allocate_and_fill(size_type n,const T& value){
+    start = data_allocator::allocate(n);
+    finish = uninitialized_fill_n(start,n,value);
+    end_of_storage = start + n;
+}
+
+/*配置空间并复制内容,返回容器开始的迭代器*/
+template <class T, class Alloc>
+vector<T,Alloc>::iterator vector<T,Alloc>::allocate_and_copy(iterator first,iterator last){
+    difference_type n = last - first;
+    start = data_allocator::allocate(n);
+    finish = uninitialized_copy(first,last,start);
+    end_of_storage = finish;
+    return start;
+}
 
 // insert_aux 函数
 template <class T, class Alloc>
@@ -339,10 +302,7 @@ void vector<T, Alloc>::insert_aux(iterator position, const T& x){
             data_allocator::deallocate(new_start,new_size);
             throw;
         }
-
-        destory(start,finish);
-        this->deallocate();
-
+        destory_and_deallocate();
         start = new_start;
         finish = new_finish;
         end_of_storage = start + new_size;
@@ -384,17 +344,43 @@ void vector<T, Alloc>::insert(iterator position, size_type n,const T& x){
             data_allocator::deallocate(new_finish,new_size);
             throw;
         }
-
-        destory(start,finish);
-        this->deallocate();
-
+        destory_and_deallocate();
         start = new_start;
         finish = new_finish;
         end_of_storage = start + new_size;
     }
 }
 
-}//namespace ZMJ
 
+template<class T,class Alloc>
+inline bool operator==(vector<T,Alloc>& x,vector<T,Alloc>& y){
+    return x.size() == y.size() && equal(x.begin(),x.end(),y.begin());
+}
 
-#endif //vector.h
+template<class T,class Alloc>
+inline bool operator<(vector<T,Alloc>& x,vector<T,Alloc>& y){
+    return lexicographical_compare(x.begin(),x.end(),y.begin(),y.end());
+}
+
+template<class T,class Alloc>
+inline bool operator==(vector<T,Alloc>& x,vector<T,Alloc>& y){
+    return !(x == y);
+}
+
+template<class T,class Alloc>
+inline bool operator>(vector<T,Alloc>& x,vector<T,Alloc>& y){
+    return y < x;
+}
+
+template<class T,class Alloc>
+inline bool operator>=(vector<T,Alloc>& x,vector<T,Alloc>& y){
+    return !(x < y);
+}
+
+template<class T,class Alloc>
+inline bool operator<=(vector<T,Alloc>& x,vector<T,Alloc>& y){
+    return !(y < x);
+}
+
+}// namespace ZMJ
+#endif // ！__VECTOR_H__
