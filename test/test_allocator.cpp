ca33115c2dbd362f239eb6bbaa38395bab9673b8
luna <lunasumshine@gmail.com>
2024-01-02 19:35:29 +0800

    update vecotr,allcator test

--- test/test_allocator.cpp
+++ test/test_allocator.cpp
@@ -1,5 +1,7 @@
 #include "../allocator.h"
 #include "../sgi_allocator.h"
+#include "algo.h"
+#include "vector.h"
 
 #include <string>
 #include <list>
@@ -28,7 +30,7 @@ int main(){
     //for vector
     int v[] = {1,3,4,13203,2302,23,34,3,53,23,23};
     std::vector<int,ZMJ::simple_alloc<int>> vec(v,v+sizeof(v)/sizeof(int));
-    for_each(vec.begin(),vec.end(),[](int i){ cout<<i<<" "; });
+    std::for_each(vec.begin(),vec.end(),[](int i){ cout<<i<<" "; });
     std::cout<<std::endl;
 
     //for map
@@ -40,7 +42,7 @@ int main(){
         std::string tmp = base + itoa(i,buf,10);
         mp.insert({tmp,16});
     }
-    for_each(mp.begin(),mp.end(),[](std::pair<std::string,int> i){ cout<<i.first<<" "<<i.second<<"\n"; });
+    std::for_each(mp.begin(),mp.end(),[](std::pair<std::string,int> i){ cout<<i.first<<" "<<i.second<<"\n"; });
 
     //for unordered_map : 这里还要给simple_alloc提供一个拷贝构造！simple_alloc(const simple_alloc<U>& other);
     std::unordered_map<std::string,int,hash<std::string>,equal_to<std::string>,ZMJ::simple_alloc<std::pair<const std::string,int>>> umap;
@@ -49,25 +51,26 @@ int main(){
         std::string tmp = base + itoa(i,buf,10);
         umap.insert({tmp,16});
     }
-    for_each(umap.begin(),umap.end(),[](std::pair<std::string,int> i){ cout<<i.first<<" "<<i.second<<"\n"; });
+    std::for_each(umap.begin(),umap.end(),[](std::pair<std::string,int> i){ cout<<i.first<<" "<<i.second<<"\n"; });
 
     //for list
     std::list<std::string,ZMJ::simple_alloc<std::string>> lt;
     for(int i = 0; i < 1000; ++i){
         lt.push_back("YOLA");
     }
-    for_each(lt.begin(),lt.end(),[](std::string& i){ cout<<i<<"\n"; });
+    std::for_each(lt.begin(),lt.end(),[](std::string& i){ cout<<i<<"\n"; });
 
     //for dequeue
     std::deque<std::string,ZMJ::simple_alloc<std::string>> dt;
     for(int i = 0; i < 1000; ++i){
         dt.push_back("RaidenEi");
     }
-    for_each(dt.begin(),dt.end(),[](std::string& i){ cout<<i<<"\n"; });
+    std::for_each(dt.begin(),dt.end(),[](std::string& i){ cout<<i<<"\n"; });
 
     //for big memory allocate.
     BigBlock* big = ZMJ::simple_alloc<BigBlock>().allocate(2);
     big->set(628);
     ZMJ::simple_alloc<BigBlock>().deallocate(big,2);
+
     return 0;
 }
---
+++ test/test_vector.cpp
@@ -0,0 +1,11 @@
+#include "vector.h"
+#include "algo.h"
+#include <iostream>
+
+using namespace ZMJ;
+
+int main(){
+    vector<int> nums(10,2);
+    vector<int> nums1(nums.begin(),nums.end());
+    for_each(nums1.begin(),nums1.end(),[](int i){ std::cout<<i<<" ";});
+}
